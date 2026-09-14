96d262126a7585ba17c2dfd82d683ad128bbcd12
Eylul Guvenlik <info@eylulguvenlik.com>
2026-09-14 21:12:47 +0300

    Increase logo size

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
       <img
         src={`${import.meta.env.BASE_URL}logo.webp`}
         alt="Eylül Elektronik"
-        className="h-20 md:h-32 lg:h-48 w-auto object-contain transition-all duration-300"
+        className="h-28 md:h-40 lg:h-48 w-auto object-contain transition-all duration-300"
       />
     </div>
   );
